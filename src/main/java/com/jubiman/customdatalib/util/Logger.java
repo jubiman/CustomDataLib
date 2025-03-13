d8241da393cd815bf8665ae7cafe0532b54cc713
jubiman <bobvries111@gmail.com>
2025-03-13 18:18:59 +0100

    Fix newline for logging

--- src/main/java/com/jubiman/customdatalib/util/Logger.java
+++ src/main/java/com/jubiman/customdatalib/util/Logger.java
@@ -4,23 +4,23 @@ import necesse.engine.GameLog;
 
 public class Logger {
 	public static void info(String message, Object... args) {
-		GameLog.out.format("[CustomDataLib] %s", String.format(message, args));
+		GameLog.out.format("[CustomDataLib] %s\n", String.format(message, args));
 	}
 
 	public static void error(String message, Object... args) {
-		GameLog.err.format("[CustomDataLib] %s", String.format(message, args));
+		GameLog.err.format("[CustomDataLib] %s\n", String.format(message, args));
 	}
 
 	public static void warn(String message, Object... args) {
-		GameLog.warn.format("[CustomDataLib] %s", String.format(message, args));
+		GameLog.warn.format("[CustomDataLib] %s\n", String.format(message, args));
 	}
 
 	public static void debug(String message, Object... args) {
-		GameLog.debug.format("[CustomDataLib] %s", String.format(message, args));
+		GameLog.debug.format("[CustomDataLib] %s\n", String.format(message, args));
 	}
 
 	public static void file(String message, Object... args) {
-		GameLog.file.format("[CustomDataLib] %s", String.format(message, args));
+		GameLog.file.format("[CustomDataLib] %s\n", String.format(message, args));
 	}
 }
 
